2f8af5b0c1886164fa144f592928e87a08a95b1e
juanagudelop <juanpagudelop@gmail.com>
2025-08-04 16:46:13 -0500

    Pantalla de carga al minijuego nacer

--- Assets/Scripts/UI/MenuPrincipal.cs
+++ Assets/Scripts/UI/MenuPrincipal.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public GameObject pantallaTransicion;
+
     public void Jugar()
     {
+        StartCoroutine(MostrarTransicionYJugar());
+    }
+
+    IEnumerator MostrarTransicionYJugar()
+    {
+        pantallaTransicion.SetActive(true);
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
